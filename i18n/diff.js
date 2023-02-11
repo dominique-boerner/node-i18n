b13fce847743ee802696d7cc362e18b4a5582890
Dominique Börner <dominique@mukosoft.de>
2023-02-11 12:03:58 +0100

    basic implementation for diff

--- i18n/diff.js
+++ i18n/diff.js
@@ -1,4 +1,5 @@
 const { flattenObject } = require("../util");
+const util = require("util");
 
 /**
  * The diff() function takes the content of n files, and returns the difference between
@@ -9,16 +10,41 @@ const { flattenObject } = require("../util");
  * @return {string[]} difference
  */
 module.exports = function diff(fileContents, i18nFiles) {
+  let languages = new Set();
   // TODO: WIP
   const flattenedObjectKeys = fileContents.map((content) => {
     return Object.keys(flattenObject(content));
   });
 
-  let i18nWithTranslation = {};
+  i18nFiles.forEach((i18n, index) => {
+    languages.add({
+      language: i18n,
+      data: flattenedObjectKeys[index],
+    });
+  });
 
-  i18nFiles.forEach(
-    (file, index) => (i18nWithTranslation[file] = flattenedObjectKeys[index])
+  const buffers = [...languages].map((content) =>
+    Buffer.from(JSON.stringify(content.data))
   );
 
-  return i18nWithTranslation;
+  let compared = new Set();
+  buffers.forEach((buffer, index) => {
+    const buffersCopy = [...buffers];
+    delete buffersCopy[index];
+
+    buffersCopy.forEach((bufferCopy, index2) => {
+      if (buffer.compare(bufferCopy) !== 0) {
+        console.log(
+          i18nFiles[index],
+          buffer.toString(),
+          "is not equal to",
+          i18nFiles[index2],
+          bufferCopy.toString()
+        );
+        compared.add(buffer);
+      }
+    });
+  });
+
+  return [...compared];
 };
